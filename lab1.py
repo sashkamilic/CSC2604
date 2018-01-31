dafcba545b5398bdcf155ce31de69e60e22130ff
Ubuntu <class_test@ClassTest.3qsff0l4vsoe5b3f1lasdidqgg.bx.internal.cloudapp.net>
2018-01-31 08:43:41 +0000

    added additional pmi variants

--- lab1.py
+++ lab1.py
@@ -1,31 +1,37 @@
+import itertools
 import nltk
 import re
 import scipy
+from tabulate import tabulate
 from collections import Counter, defaultdict
 from nltk.corpus import brown, reuters, stopwords
 from nltk.util import ngrams
 from multiprocessing import Pool
 #import pandas as pd
-from pandas import *
+
+import numpy as np
+import pandas as pd
 from numpy import log
 import time
 from functools import partial
+from sklearn.decomposition import TruncatedSVD
 
+import sys
 #np.set_printoptions(threshold=np.nan)
 
 SIM_FILE = "RG_word_sims.tsv"
-NUM_PROCESSES = 8
 
-def m1(corpus, n, include_words=None):
+def m1(corpora, n, include_words=None):
     '''
     Construct a word-context vector model by collecting
-    bigram counts for top-n words in corpus.
+    bigram counts for top-n words in the corpora.
 
     `include_words` - optional list of words to include along
     with top-n
     '''
+    corpora_iter = itertools.chain.from_iterable([c.words() for c in corpora])
     # Extract the 5000 most common English words
-    freq_dict = nltk.FreqDist(w.lower() for w in corpus.words())
+    freq_dict = nltk.FreqDist(w.lower() for w in corpora_iter)
     # remove non numeric from freq_dict
     keys = list(freq_dict.keys())
     for k in keys:
@@ -33,10 +39,12 @@ def m1(corpus, n, include_words=None):
             del freq_dict[k]
 
     W = list(dict(freq_dict.most_common(n)).keys())
-    W.extend(include_words)
+    if include_words:
+        W.extend(include_words)
 
     # Get bigrams from words in W
-    bigrams = ngrams([w.lower() for w in corpus.words()], 2)
+    corpora_iter = itertools.chain.from_iterable([c.words() for c in corpora])
+    bigrams = ngrams([w.lower() for w in corpora_iter], 2)
     # filter out words not in W
     bigrams = [bi for bi in bigrams if bi[0] in W and bi[1] in W]
 
@@ -46,7 +54,7 @@ def m1(corpus, n, include_words=None):
     for t,freq in c.items():
         d[t[1]][t[0]] = freq
 
-    M1 = DataFrame(d).fillna(0).to_sparse()
+    M1 = pd.DataFrame(d).fillna(0).to_sparse()
 
     # remove words not in both rows and columns
     M1 = M1.drop(list(set(M1.index) - set(M1.columns.values)))
@@ -55,12 +63,20 @@ def m1(corpus, n, include_words=None):
     return M1
 
 
-def pmi(M):
+def pmi(M, k=1, normalized=False):
+    '''
+    If k > 1, compute PMI^k
+    (see: "Handling the Impact of Low frequency Events..." (2011))
+    '''
     # +1 for smoothing
     P_xy = (M + 1) / M.values.sum()
+    P_xy = P_xy.pow(k)
     P_x = (M.sum(axis=1) + 1) / M.values.sum()
     P_y = (M.sum(axis=0) + 1) / M.values.sum()
-    return np.log(P_xy.div(P_x, axis=1).div(P_y, axis=0))
+    pmi = np.log(P_xy.div(P_x, axis=1).div(P_y, axis=0))
+    if normalized:
+        return pmi.div(-np.log(P_xy))
+    return pmi
 
 
 def test(M):
@@ -68,6 +84,7 @@ def test(M):
     Compute Pearson correlation between similarity scores in
     Rubenstein and Goodenougth (1965) and cosine similarities in M
     '''
+    indices = M.index.values
     filelines = [line.strip().split() for line in open(SIM_FILE).readlines()]
     sims_dict = dict([((w1,w2), float(s)) for (w1,w2,s) in filelines])
 
@@ -76,16 +93,17 @@ def test(M):
 
     for (w1, w2) in sims_dict.keys():
 
-        if w1 not in M:
+        if w1 not in indices:
             #print('"{}" not in matrix'.format(w1))
             continue
 
-        if w2 not in M:
+        if w2 not in indices:
             #print('"{}" not in matrix'.format(w2))
             continue
 
-        v1 = np.array(M[w1])
-        v2 = np.array(M[w2])
+        v1 = np.array(M.loc[[w1]])
+        v2 = np.array(M.loc[[w2]])
+
         x.append(scipy.spatial.distance.cosine(v1, v2))
         y.append(sims_dict[(w1, w2)])
 
@@ -95,24 +113,47 @@ def test(M):
     return scipy.stats.pearsonr(x, y)
 
 
-def get_col_name(row):
-    b = (df.ix[row.name] == row['value'])
-    return b.index[b.argmax()]
-
-def filter_nonW(W, l):
-    list(filter(lambda t: t[0] in W and t[1] in W, l))
-
 if __name__ == "__main__":
 
-    freq_dict = nltk.FreqDist(w.lower() for w in brown.words())
-    words = set([line.split()[0] for line in open(SIM_FILE).readlines()])
-    # include words (freq > 10) from R&G sim list along with top-5000 in M1
-    additional_words = [k for k in words if freq_dict[k] >= 5]
-    print(additional_words)
-    M1 = m1(brown, 5000, include_words=additional_words)
+    #corpora_iter = itertools.chain.from_iterable([c.words() for c in [brown, reuters]])
+    #freq_dict = nltk.FreqDist(w.lower() for w in corpora_iter)
+    #words = [line.split()[0] for line in open(SIM_FILE).readlines()]
+    #include_words = [w for w in words if freq_dict[w] >= 15]
+    #M1 = m1([brown, reuters], 5000, include_words=include_words)
     #M1.to_pickle('m1.pkl')
-    M1 = pandas.read_pickle('m1.pkl').to_dense()
+
+    # co-occurrance matrix
+    M1 = pd.read_pickle('m1.pkl').to_dense()
+    # pmi
     M1_plus = pmi(M1)
-    # try averaging cosign of context and word vectors (i.e. row/column vectors)
-    print(test(M1))
-    print(test(M1_plus))
+    # pmi^2
+    M1_pmi2 = pmi(M1, 2)
+    # npmi
+    M1_npmi = pmi(M1, normalized=True)
+    # ppmi
+    M1_ppmi = M1_plus.copy()
+    M1_ppmi[M1_ppmi < 0] = 0
+
+    names = ['raw', 'pmi', 'pmi^2', 'npmi', 'ppmi']
+    Ms = [M1, M1_plus, M1_pmi2, M1_npmi, M1_ppmi]
+
+    header = ['measure', 'M1', 'M2_10', 'M2_20', 'M2_50', 'M2_100']
+    table = []
+    for name, M in zip(names, Ms):
+
+        result = [name]
+        # first test on non-truncated matrix
+        corr, pvalue = test(M)
+        result.append('{0:.3f} ({0:.3f})'.format(corr, pvalue))
+
+        for k in [10, 20, 50, 100]:
+            svd = TruncatedSVD(n_components=k, algorithm="arpack")
+            M_trunc = svd.fit_transform(scipy.sparse.csr_matrix(M))
+            M_trunc = pd.DataFrame(data=M_trunc, index=M1.index.values)
+            corr, pvalue = test(M_trunc)
+            result.append('{0:.3f} ({0:.3f})'.format(corr, pvalue))
+
+        table.append(result)
+
+    print(tabulate(table, header, tablefmt="fancy_grid"))
+
